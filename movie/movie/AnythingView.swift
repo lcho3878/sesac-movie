b77844cc7ff3feeb91c30cee722c02dfb67692ec
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-10-16 14:13:30 +0900

    AnythingView

---
+++ movie/movie/AnythingView.swift
@@ -0,0 +1,14 @@
+//
+//  AnythingView.swift
+//  movie
+//
+//  Created by 이찬호 on 10/16/24.
+//
+
+import SwiftUI
+
+struct AnythingView: View {
+    var body: some View {
+        Text("anyany")
+    }
+}
